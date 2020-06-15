fd01bd1ca4f92a3b64d7332d2ea7b6e70408ecd5
jjfigueroa <josejaime.figueroa@molex.com>
2020-06-15 16:10:17 -0600

    Challenge 10. Done

---
+++ Javascript/challenge10.js
@@ -0,0 +1,9 @@
+console.log('Challenge 10');
+
+const miles = prompt('Enter some quantity of miles, in order to convert to Kilometers.');
+
+const OneMile = 1.609344;
+
+let total = miles * OneMile;
+
+console.log(`${miles} Mile(s) equal to ${parseFloat(total)} kilometer(s)`);
